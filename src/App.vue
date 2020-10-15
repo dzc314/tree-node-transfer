a4e8e3a19cdcd09371257f71cd6115bf0201a16b
daizhongchu <dzc314@163.com>
2020-10-15 17:19:07 +0800

    DEMO

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,94 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" @click="showData"/>
+    <TreeNodeTransfer :source="source" :target="target" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TreeNodeTransfer from "./components/TreeNodeTransfer.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    TreeNodeTransfer,
+  },
+  data() {
+    return {
+      source: [
+        {
+          code: 1,
+          parendCode: null,
+          isLeaf:false,
+          name: "一级",
+          children: [
+            {
+              code: 2,
+              parendCode: 1,
+              isLeaf:false,
+              name: "二级1",
+              children: [
+                {
+                  code: 3,
+                  parendCode: 2,
+                  isLeaf:false,
+                  name: "三级1.1",
+                  children: []
+                },
+                {
+                  code: 4,
+                  parendCode: 2,
+                  isLeaf:false,
+                  name: "三级1.2",
+                },
+              ],
+            },
+            {
+              code: 5,
+              parendCode: 1,
+              isLeaf:false,
+              name: "二级2",
+              children: [
+                {
+                  code: 6,
+                  parendCode: 2,
+                  isLeaf:false,
+                  name: "三级2.1",
+                },
+                {
+                  code: 7,
+                  parendCode: 2,
+                  isLeaf:false,
+                  name: "三级2.2",
+                },
+              ],
+            },
+            {
+              code: 8,
+              parendCode: 1,
+              isLeaf:false,
+              name: "二级3",
+              children: []
+            },
+          ],
+        },
+      ],
+      target: [
+        {
+          code: 8,
+          parendCode: 1,
+          isLeaf:false,
+          name: "二级3",
+        },
+      ],
+    };
+  },
+  methods: {
+    showData() {
+      console.log(this.target);
+    }
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/TreeNodeTransfer.vue
@@ -0,0 +1,150 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :span="12">
+      <el-tree
+        ref="tree"
+        :data="source"
+        :props="props"
+        node-key="code"
+        show-checkbox
+        default-expand-all
+        :filter-node-method="filterNode"
+        @check="handleCheck"
+      >
+        <div class="custom-tree-node" slot-scope="{ node, data }">
+          <span>{{ data.name }}</span>
+          <i
+            v-if="!data.isLeaf && !data.children"
+            class="el-icon-caret-bottom"
+            @click="loadNodeChildren(node, data)"
+          ></i>
+        </div>
+      </el-tree>
+    </el-col>
+    <el-col :span="12">
+      <ul>
+        <li v-for="(item, index) in target" :key="index">
+          <span>{{ item.name }}</span
+          ><i class="el-icon-circle-close" @click="targetHandle(index)"></i>
+        </li>
+      </ul>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: "TreeNodeTransfer",
+  props: {
+    source: {
+      type: Array,
+      default: () => [],
+    },
+    target: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  model: {
+    prop: "target",
+    event: "change",
+  },
+  data() {
+    return {
+      props: {
+        label: "name",
+      },
+    };
+  },
+  watch: {
+    target: {
+      handler: function (val) {
+        this.$refs.tree.filter(val);
+      },
+      deep: true,
+    },
+  },
+
+  mounted() {
+    this.renderSourceTree(this.source);
+    this.$refs.tree.filter(null);
+  },
+  methods: {
+    renderSourceTree(nodes) {
+      if (!nodes.length) {
+        return;
+      }
+      const target = this.target;
+      nodes.forEach((node) => {
+        let nodeSelected = false;
+        if (!node.isLeaf && !node.children) {
+          this.$set(node, "disabled", true);
+        }
+        if (node.children && node.children.length) {
+          this.renderSourceTree(node.children);
+          const isChildrenAllSelected = node.children.every(
+            (child) => !child.visible
+          );
+          nodeSelected = isChildrenAllSelected;
+        } else {
+          nodeSelected = target.some((item) => {
+            return item.code === node.code;
+          });
+        }
+        node.visible = !nodeSelected;
+      });
+    },
+
+    sourceToTarget(data) {
+      if (data.children && data.children.length) {
+        data.children.forEach((child) => {
+          this.sourceToTarget(child);
+        });
+      } else {
+        this.target.push(data);
+      }
+    },
+    handleCheck(data) {
+      // console.log(data);
+      data.visible = false;
+      this.sourceToTarget(data);
+      this.renderSourceTree(this.source);
+      this.$refs.tree.setCheckedKeys([]);
+    },
+    filterNode(value, data) {
+      return data.visible;
+    },
+    targetHandle(index) {
+      this.target.splice(index, 1);
+      this.renderSourceTree(this.source);
+    },
+    loadNodeChildren(node, data) {
+      if (!data.children) {
+        this.$set(data, "children", []);
+      }
+    //   模拟请求数据
+      setTimeout(() => {
+        data.children = [
+          {
+            name: "test",
+            code: Math.round(Math.random() * 10000),
+            isLeaf: Math.random() > 0.5,
+          },
+          {
+            name: "test",
+            code: Math.round(Math.random() * 10000),
+            isLeaf: Math.random() > 0.5,
+          },
+        ];
+        // 重新渲染树以匹配已选择数据
+        this.renderSourceTree(this.source);
+        // 默认全部展开则不需要设置节点展开
+        // node.expanded = true;
+      }, 1000);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
